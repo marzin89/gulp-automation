7b0d879adb01e0f4616f5daf0e892201f13a6f76
marco <mazi2001@student.miun.se>
2021-09-11 12:27:45 +0200

    Add task that concatenates, minifies and copies CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 // Inkludera Gulp (objekt med funktioner)
 const { src, dest, watch, series, parallel } = require('gulp');
+// Inkludera gulp-concat
+const concat = require('gulp-concat');
+// Inkludera gulp-cssnano
+const cssnano = require('gulp-cssnano');
 // Sökvägar till HTML, CSS, JS och bilder
 const paths = {
     html: 'src/**/*.html',
@@ -13,7 +17,14 @@ function htmlTask() {
     return src(paths.html)
     .pipe(dest('pub'));
 }
+
+function cssTask() {
+    return src(paths.css)
+    .pipe(concat('styles.css'))
+    .pipe(cssnano())
+    .pipe(dest('pub/css'));
+}
 // Exportera tasks
 exports.default = series(
-    parallel(htmlTask)
+    parallel(htmlTask, cssTask)
 )
